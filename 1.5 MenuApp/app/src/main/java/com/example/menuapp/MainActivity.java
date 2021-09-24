f329ac0765d5aa32a579cd99a895cb3d11cb9cf1
Kirill <202536@edu.fa.ru>
2021-09-24 13:41:19 +0300

    Menu app added

---
+++ 1.5 MenuApp/app/src/main/java/com/example/menuapp/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.menuapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    CheckBox cb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.textView);
+        cb = findViewById(R.id.check);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu){
+        menu.setGroupVisible(R.id.group1, cb.isChecked());
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Item Menu");
+        sb.append("\r\n groupId: " + String.valueOf(item.getGroupId()));
+        sb.append("\r\n itemId: " + String.valueOf(item.getItemId()));
+        sb.append("\r\n order: " + String.valueOf(item.getOrder()));
+        sb.append("\r\n title: " + item.getTitle());
+        tv.setText(sb.toString());
+        return super.onOptionsItemSelected(item);
+
+    }
+
+}
